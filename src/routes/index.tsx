96f29a2c098742cfc256926f361b6f580067bea5
ben10pedrin <pedro123ben10@gmail.com>
2023-07-16 23:02:29 -0700

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+     <nav>
+      ToDo's
+           <button>Add</button>
+     </nav>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
